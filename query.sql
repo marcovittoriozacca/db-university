b6151aa2abf535c7f3326b3262fd274f7087ac87
marcovittoriozacca <zaccamarco01@gmail.com>
2024-03-04 12:43:55 +0100

    feat: GROUP BY query

--- query.sql
+++ query.sql
@@ -76,4 +76,52 @@ WHERE `phone` IS NULL;
 /*------------------------------------------------------------------------------*/
 
 
+/*                  QUERY CON GROUPBY                  */
+
+/* 1. Contare quanti iscritti ci sono stati ogni anno */
+
+SELECT COUNT('id') as 'ISCRITTI_OGNI_ANNO', YEAR(`enrolment_date`)
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio */
+
+SELECT COUNT('id') as 'INSEGNANTI_NELLO_STESSO_UFFICIO', `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 3. Calcolare la media dei voti di ogni appello d'esame */
+
+SELECT `exam_id`, AVG(`vote`)
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+/* se si vuole arrotondare il voto: */
+
+/*
+SELECT `exam_id`, ROUND(AVG(`vote`))
+FROM `exam_student`
+GROUP BY `exam_id`;
+*/
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 4. Contare quanti corsi di laurea ci sono per ogni dipartimento */
+
+SELECT `department_id`, COUNT(`name`)
+FROM `degrees`
+GROUP BY `department_id`;
+
+/*------------------------------------------------------------------------------*/
+
+
+
+
 
