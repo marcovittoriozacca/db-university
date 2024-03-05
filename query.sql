67f74f6c558c19e3b04bc15caba008e223f75524
marcovittoriozacca <zaccamarco01@gmail.com>
2024-03-05 12:14:46 +0100

    feat: JOIN query

--- query.sql
+++ query.sql
@@ -122,6 +122,89 @@ GROUP BY `department_id`;
 /*------------------------------------------------------------------------------*/
 
 
+/*                  QUERY CON JOIN                  */
 
+
+/* 1. Seleziona tutti gli studenti iscritti al Corso di Laurea in Economia */
+
+SELECT *
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze */
+
+SELECT * 
+FROM `degrees`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `degrees`.`level` = "magistrale"
+AND `departments`.`name` = "Dipartimento di Neuroscienze";
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) */
+
+SELECT *
+FROM `courses`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `teachers`.`name` = "Fulvio" AND `teachers`.`surname` = "Amato"
+OR `teachers`.`id` = 44;
+
+/* Il controllo OR non era necessario ma abbiamo una sicurezza in più in caso dovessero esserci problemi relativi al nome */
+
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome */
+
+SELECT *
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`ASC, `students`.`name` ASC;
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 5. Seleziona tutti i corsi di laurea con i relativi corsi e insegnanti */
+
+SELECT *
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`;
+
+/*------------------------------------------------------------------------------*/
+
+
+/* 6. Selezionare tutti i docenti che insegnano nel dipartimento di Matematica (54) */
+
+
+SELECT `teachers`.`id`, `teachers`.`name`, `teachers`.`surname`, `departments`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = "Dipartimento di Matematica";
 
 
